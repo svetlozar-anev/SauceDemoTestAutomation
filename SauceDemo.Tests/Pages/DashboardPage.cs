ca4553748b3c415ae7b3e514a15beceeb0b14020
IO-DEVICE\svetl <svetlozar.anev@gmail.com>
2025-06-26 13:48:44 +0300

    Codesmell cleanup - appsettings, stylecop, editorconfig added.

--- SauceDemo.Tests/Pages/DashboardPage.cs
+++ SauceDemo.Tests/Pages/DashboardPage.cs
@@ -1,8 +1,12 @@
-﻿using OpenQA.Selenium;
-using SauceDemo.Tests.Base;
+﻿// <copyright file="DashboardPage.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace SauceDemo.Tests.Pages
 {
+    using OpenQA.Selenium;
+    using SauceDemo.Tests.Base;
+
     /// <summary>
     /// Page Object Model for the post-login Dashboard page ("Swag Labs").
     /// Validates successful login by checking for known elements or title.
@@ -10,23 +14,24 @@ namespace SauceDemo.Tests.Pages
     public class DashboardPage : BasePage
     {
         // === LOCATORS ===
-
-        private readonly By AppLogo = By.CssSelector(".app_logo"); // Common identifier on the dashboard
+        private readonly By appLogo = By.CssSelector(".app_logo"); // Common identifier on the dashboard
 
         /// <summary>
         /// Checks if the user is on the Swag Labs dashboard by verifying the logo is visible.
         /// </summary>
+        /// <returns>True if the dashboard logo is visible; otherwise, false.</returns>
         public bool IsAtDashboard()
         {
-            return IsElementDisplayed(AppLogo);
+            return this.IsElementDisplayed(this.appLogo);
         }
 
         /// <summary>
         /// Gets the current page title.
         /// </summary>
+        /// <returns>The title of the current browser page.</returns>
         public string GetPageTitle()
         {
-            return Driver.Title;
+            return this.Driver.Title;
         }
     }
 }
--- SauceDemo.Tests/Tests/LoginTests.cs
+++ SauceDemo.Tests/Tests/LoginTests.cs
@@ -1,9 +1,16 @@
-﻿using FluentAssertions;
-using SauceDemo.Tests.Pages;
-using SauceDemo.Tests.Utilities;
+﻿// <copyright file="LoginTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace SauceDemo.Tests.Tests
 {
+    using FluentAssertions;
+    using SauceDemo.Tests.Pages;
+    using SauceDemo.Tests.Utilities;
+
+    /// <summary>
+    /// Contains automated UI tests related to login functionality for SauceDemo.
+    /// </summary>
     [TestFixture]
     [Parallelizable(ParallelScope.Self)]
     public class LoginTests
@@ -11,48 +18,67 @@ namespace SauceDemo.Tests.Tests
         private LoginPage loginPage;
         private DashboardPage dashboardPage;
 
+        /// <summary>
+        /// Initializes the WebDriver and page objects before each test.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
-            WebDriverFactory.InitDriver(); // TODO: or read from config
-            loginPage = new LoginPage();
-            dashboardPage = new DashboardPage();
-            loginPage.Open();
+            WebDriverFactory.InitDriver(Config.TestConfig.Browser);
+            this.loginPage = new LoginPage();
+            this.dashboardPage = new DashboardPage();
+            this.loginPage.Open();
         }
 
+        /// <summary>
+        /// Cleans up the WebDriver after each test.
+        /// </summary>
         [TearDown]
         public void TearDown()
         {
             WebDriverFactory.QuitDriver();
         }
 
+        /// <summary>
+        /// Verifies that submitting the login form with both username and password empty
+        /// shows the appropriate 'Username is required' error message.
+        /// </summary>
         [Test]
         [Description("UC-1: Login attempt with empty credentials shows 'Username is required' error")]
         public void Login_WithEmptyCredentials_ShowsUsernameRequiredError()
         {
-            loginPage.EnterUsername("standard_user");
-            loginPage.EnterPassword("secret_sauce");
-            loginPage.ClearUsername();
-            loginPage.ClearPassword();
+            this.loginPage.EnterUsername("standard_user");
+            this.loginPage.EnterPassword("secret_sauce");
+            this.loginPage.ClearUsername();
+            this.loginPage.ClearPassword();
 
-            loginPage.ClickLogin();
+            this.loginPage.ClickLogin();
 
-            loginPage.GetErrorMessage().Should().Be("Epic sadface: Username is required");
+            this.loginPage.GetErrorMessage().Should().Be("Epic sadface: Username is required");
         }
 
+        /// <summary>
+        /// Verifies that submitting the login form without a password
+        /// shows the appropriate 'Password is required' error message.
+        /// </summary>
         [Test]
         [Description("UC-2: Login attempt without password shows 'Password is required' error")]
         public void Login_WithMissingPassword_ShowsPasswordRequiredError()
         {
-            loginPage.EnterUsername("standard_user");
-            loginPage.EnterPassword("secret_sauce");
-            loginPage.ClearPassword();
+            this.loginPage.EnterUsername("standard_user");
+            this.loginPage.EnterPassword("secret_sauce");
+            this.loginPage.ClearPassword();
 
-            loginPage.ClickLogin();
+            this.loginPage.ClickLogin();
 
-            loginPage.GetErrorMessage().Should().Be("Epic sadface: Password is required");
+            this.loginPage.GetErrorMessage().Should().Be("Epic sadface: Password is required");
         }
 
+        /// <summary>
+        /// Verifies that valid login credentials navigate the user to the dashboard page.
+        /// </summary>
+        /// <param name="username">The username to be used for login.</param>
+        /// <param name="password">The password to be used for login.</param>
         [Test]
         [TestCase("standard_user", "secret_sauce")]
         [TestCase("problem_user", "secret_sauce")]
@@ -60,10 +86,10 @@ namespace SauceDemo.Tests.Tests
         [Description("UC-3: Successful login with accepted credentials navigates to dashboard")]
         public void Login_WithValidCredentials_NavigatesToDashboard(string username, string password)
         {
-            loginPage.Login(username, password);
+            this.loginPage.Login(username, password);
 
-            dashboardPage.IsAtDashboard().Should().BeTrue("the user should be redirected to the dashboard");
-            dashboardPage.GetPageTitle().Should().Be("Swag Labs");
+            this.dashboardPage.IsAtDashboard().Should().BeTrue("the user should be redirected to the dashboard");
+            this.dashboardPage.GetPageTitle().Should().Be("Swag Labs");
         }
     }
 }
